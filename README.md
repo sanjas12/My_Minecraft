972a98e889ba364c026247c648c5d0b8bbc01e23
sanja_s <sanja100v@gmail.com>
2023-02-06 21:11:30 +0300

    Update

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # My_Minecraft
+https://education.minecraft.net/en-us/lessons/python-101-lesson-1
--- lesson_01.py
+++ lesson_01.py
@@ -1,2 +1,9 @@
-print("Yarik_sent")
-print("HI_WORLDofTANKS")
+# print("Yarik_sent")
+# print("HI_WORLDofTANKS")
+# print("56")
+# print("56")
+# print("56")
+# print("56")
+
+for i in range(10):
+    print(i,"cycle")
